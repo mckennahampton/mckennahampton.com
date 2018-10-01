5a350b3064d6ae170752d388735dcd66264a5395
McKenna Hampton <mckenna@mckennahampton.com>
2018-09-30 22:56:14 -0500

    Working on layerslider

--- wp-content/themes/wp-bootstrap-4-child/functions.php
+++ wp-content/themes/wp-bootstrap-4-child/functions.php
@@ -1,1 +1,55 @@
 <?php
+
+//remove the default theme stuff
+remove_action( 'wp_enqueue_scripts', 'wp_bootstrap_4_scripts' );
+
+//remove the default emoji crap
+
+function grd_remove_emoji() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'grd_remove_tinymce_emoji' );
+}
+add_action( 'init', 'grd_remove_emoji' );
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function grd_remove_tinymce_emoji( $plugins ) {
+	if ( ! is_array( $plugins ) ) {
+		return array();
+	}
+	return array_diff( $plugins, array( 'wpemoji' ) );
+}
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+add_filter('upload_mimes', 'cc_mime_types');
+
+wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css');
+
+wp_deregister_script('jquery');
+
+wp_register_script( 'bs-script', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
+wp_register_script('jquery', 'https://code.jquery.com/jquery-3.3.1.min.js');
+wp_register_script('greensock', get_stylesheet_directory_uri() . '/js/greensock.js');
+wp_register_script('ls-kreaturamedia', get_stylesheet_directory_uri() . '/js/layerslider.kreaturamedia.jquery.js');
+wp_register_script('ls-transitions', get_stylesheet_directory_uri() . '/js/layerslider.transitions.js');
+wp_register_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js');
+
+
+wp_enqueue_script('jquery');
+wp_enqueue_script('greensock','','','',true);
+wp_enqueue_script('ls-transitions','','','',true);
+wp_enqueue_script('ls-kreaturamedia','','','',true);
+wp_enqueue_script('bs-script','','','',true);
+wp_enqueue_script('scripts','','','',true);
+
+?>
---
+++ wp-content/themes/wp-bootstrap-4-child/header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_4
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+
+	<header id="masthead" class="site-header <?php if ( get_theme_mod( 'sticky_header', 0 ) ) : echo 'sticky-top'; endif; ?>">
+		<div class="row large-menu">
+			<div class="hidden-xs-down order-xs-2 col-sm-4 order-sm-1 header-menu-1 align-self-end">
+				<ul>
+					<li><a class="hvr-sweep-to-left" href="/my-journey" alt="Web Journey">My Journey</a></li><!--
+					--><li><a class="hvr-sweep-to-left" href="/portfolio" alt="Portfolio">Portfolio</a></li><!--
+					--><li><a class="hvr-sweep-to-left" href="/get-a-quote" alt="Get a Quote">Get a Quote</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12 order-xs-1 col-sm-4 order-sm-2 align-self-center header-logo">
+				<a href="<?php echo site_url(); ?>" alt="Home">
+					<img alt="Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/Logo Main-01.svg">
+				</a>
+			</div>
+			<div class="hidden-xs-down col-sm-4 header-menu-2 order-sm-3 align-self-end">
+				<ul>
+					<li><a class="hvr-sweep-to-right" href="/releases" alt="Releases">Releases</a></li><!--
+					--><li><a class="hvr-sweep-to-right" href="/my-process" alt="My Process">My Process</a></li><!--
+					--><li><a class="hvr-sweep-to-right" href="/blog" alt="Blog">Blog</a></li>
+				</ul>
+			</div>
+		</div>
+	
+		<div class="row mobile-menu">
+			<div class="collapse" id="mobileNavbar">
+				<ul>
+					<li><a class="hvr-sweep-to-left" href="/my-journey" alt="Web Journey">My Journey</a></li>
+					<li><a class="hvr-sweep-to-left" href="/portfolio" alt="Portfolio">Portfolio</a></li>
+					<li><a class="hvr-sweep-to-left" href="/get-a-quote" alt="Get a Quote">Get a Quote</a></li>
+					<li><a class="hvr-sweep-to-right" href="/releases" alt="Releases">Releases</a></li>
+					<li><a class="hvr-sweep-to-right" href="/my-process" alt="My Process">My Process</a></li>
+					<li><a class="hvr-sweep-to-right" href="/blog" alt="Blog">Blog</a></li>
+				</ul>
+			</div>
+			<nav class="navbar-light">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobileNavbar" aria-controls="mobileNavbar" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+			</nav>
+		</div>
+
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
